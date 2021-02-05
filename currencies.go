5ebb7cb5d8968a7349460010391dabd13dce2fc9
Jaume Jiménez <jaume.jimenezforteza@metro-markets.de>
2021-02-05 22:44:09 +0100

    Adding first endpoint: Currencies::Ticker with some params

--- currencies.go
+++ currencies.go
@@ -4,13 +4,20 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 // Ticker returns Price, volume, market cap, and rank for all currencies across
 // 1 hour, 1 day, 7 day, 30 day, 365 day, and year to date intervals.
 // Current prices are updated every 10 seconds.
-func Ticker() []byte {
-	response, error := http.Get("https://www.google.com/robots.txt")
+func Ticker(apiKey string, params map[string]string) []byte {
+	URL, error := url.Parse("https://api.nomics.com/v1/currencies/ticker")
+	if error != nil {
+		log.Fatal("Error building API base URL")
+	}
+	buildQuery(URL, apiKey, params)
+
+	response, error := http.Get(URL.String())
 	if error != nil {
 		log.Fatal(error)
 	}
@@ -22,4 +29,16 @@ func Ticker() []byte {
 	}
 
 	return body
+}
+
+func buildQuery(URL *url.URL, apiKey string, params map[string]string) {
+	query := url.Values{}
+	query.Add("key", apiKey)
+	if _, ok := params["ids"]; ok == true {
+		query.Add("ids", params["ids"])
+	}
+	if _, ok := params["convert"]; ok == true {
+		query.Add("convert", params["convert"])
+	}
+	URL.RawQuery = query.Encode()
 }
